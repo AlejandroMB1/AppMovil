a87012677c008f2588fc78437541b8b6444417c8
Alejandro Meza <alejo5123@hotmail.com>
2020-03-13 23:35:21 -0500

    Interfaces

---
+++ DataBaseServices/Services/DataBase/IServiceDataBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseServices.Services.DataBase
+{
+    public interface IServiceDataBase<T> where T: class, new()
+    {
+        Task<List<T>> GetItemsAsync();
+        Task<T> GetItemAsync(int id);
+        Task<int> InsertItemAsync(T item);
+        Task<int> UpdateItemAsync(T item);
+        Task<int> DeleteItemAsync(T item);
+    }
+}
---
+++ DataBaseServices/Services/DataBase/IUserDataBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseServices.Services.DataBase
+{
+    public interface IUserDataBase<T> : IServiceDataBase<T> where T: class, new()
+    {
+        Task<int> InsertAllItemsAsync(IList<T> items);
+        Task<int> DeleteAllItemsAsync();
+        Task<int> GetAllItemsAsync();
+        Task<List<T>> GetAllItemsWithChildrensAsync();
+        Task InsertAllItemsWithChildrensAsync(IList<T> items);
+
+        //Task<int> InsertWithChildrensAsync(UsuarioModel item);
+    }
+}
